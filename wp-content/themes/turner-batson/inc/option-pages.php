158574acf9e29a719ccc1e8a7b694eb5fbdd826b
Will King <contact@wking.io>
2018-09-25 15:38:20 -0500

    Adding Google maps plugin

--- wp-content/themes/turner-batson/inc/option-pages.php
+++ wp-content/themes/turner-batson/inc/option-pages.php
@@ -1,23 +1,39 @@
 <?php
+/**
+ * Add option pages for CPTs
+ *
+ * @package turner-batson
+ */
 
 function add_option_page( $cpt ) {
-	if( function_exists( 'acf_add_options_page' ) ) {
-		
+	if( function_exists( 'acf_add_options_sub_page' ) ) {
 		// add sub page
 		acf_add_options_sub_page( array(
-			'page_title' 	=> 'Settings',
-			'menu_title' 	=> 'Settings',
-			'menu_slug'		=> $cpt . '-settings',
-			'parent_slug' 	=> 'edit.php?post_type=' . $cpt,
+			'page_title' 	=> $cpt . ' Settings',
+			'menu_slug'		=> strtolower( $cpt ) . '-settings',
+			'parent_slug' => 'edit.php' . ( ( 'News' === $cpt ) ? '' : '?post_type=' . strtolower( $cpt ) ),
 		) );
 	
 	}
 }
 
 function add_option_pages() {
-	add_option_page('team');
-	add_option_page('testimonial');
-	add_option_page('job');
+	add_option_page('Team');
+	add_option_page('Testimonial');
+	add_option_page('Job');
+	add_option_page('News');
+
+	if( function_exists( 'acf_add_options_page' ) ) {
+		// add sub page
+		acf_add_options_page( array(
+			'page_title' => 'Company Settings',
+			'menu_slug'	 => 'company-settings',
+			'icon_url' 	 => 'dashicons-building',
+			'position'	 => 75
+		) );
+	
+	}
+
 }
 
-add_action( 'admin_menu', 'add_option_pages' );
+add_action( 'acf/init', 'add_option_pages' );
